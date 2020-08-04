c2de4ec6e3d69e3d2bd74985e6323f28c4b9a388
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-08-04 15:50:56 +0300

    Updates

---
+++ precache-manifest.2aabcf562ca0d076944e7da2a35957d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef127db4b3dc05610e82baab18de0d2b",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "dc32ce0c2572c5766b03",
+    "url": "/snake-game/static/css/main.3a54310a.chunk.css"
+  },
+  {
+    "revision": "f9b102b813f53507bdeb",
+    "url": "/snake-game/static/js/2.9cf6b998.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game/static/js/2.9cf6b998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc32ce0c2572c5766b03",
+    "url": "/snake-game/static/js/main.0aed14c3.chunk.js"
+  },
+  {
+    "revision": "df1381353fc621194e69",
+    "url": "/snake-game/static/js/runtime-main.002c341c.js"
+  }
+]);
